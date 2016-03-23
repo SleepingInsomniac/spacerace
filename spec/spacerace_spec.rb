90de6fbd073425099f0be41eaa1ba90aeecece44
Alex Clink <code@alexclink.com>
2016-03-23 16:09:54 -0400

    Add more tests

--- spec/spacerace_spec.rb
+++ spec/spacerace_spec.rb
@@ -28,6 +28,32 @@ describe "SpaceRace" do
       expect(line).to eq("        code")
     end
     
+    ###
+    
+    it "replaces 2 spaces with 1 tab" do
+      r = SpaceRace.new(space: "  ", replacement: "\t", reduce: 2)
+      line = r.respace_line("  code")
+      expect(line).to eq("\tcode")
+    end
+    
+    it "replaces 4 spaces with 2 tabs" do
+      r = SpaceRace.new(space: "  ", replacement: "\t", reduce: 2)
+      line = r.respace_line("    code")
+      expect(line).to eq("\t\tcode")
+    end
+    
+    it "replaces 4 spaces with 1 tab" do
+      r = SpaceRace.new(space: "    ", replacement: "\t", reduce: 4)
+      line = r.respace_line("    code")
+      expect(line).to eq("\tcode")
+    end
+    
+    it "replaces 8 spaces with 2 tabs" do
+      r = SpaceRace.new(space: "    ", replacement: "\t", reduce: 4)
+      line = r.respace_line("        code")
+      expect(line).to eq("\t\tcode")
+    end
+    
   end
   
 end
